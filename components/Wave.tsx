92237d100df6f9e0e316a5906cb707cfca8d5f31
Samuel O. Asare <asare11samuel@gmail.com>
2022-10-01 22:06:56 +0000

    feat: created wave shimmer

---
+++ components/Wave.tsx
@@ -0,0 +1,60 @@
+import { LinearGradient } from 'expo-linear-gradient'
+import { useEffect, useMemo, useRef } from 'react'
+import { Animated, StyleProp, StyleSheet, View, ViewStyle } from 'react-native'
+
+interface WaveShimmerProps{
+    width: number,
+    height: number,
+    style?: StyleProp<ViewStyle>
+}
+
+export default function WaveShimmer({width, height, style}:WaveShimmerProps) {
+
+    const gradientColors = useMemo(()=>["transparent", "rgba(0,0,0,0.05)", "rgba(0,0,0,0.03)", "rgba(0,0,0,0.02)", "transparent"],[])
+    const gradientStartPosition = useMemo(()=>({x:1, y:1}),[])
+    const translateX = useRef(new Animated.Value(-width)).current
+
+    const parentContainerStyles = useMemo(()=>([styles.container,{width, height, ...style as object}]),[])
+
+    useEffect(()=>{
+        Animated.loop(
+            Animated.timing(translateX, {
+                toValue: width,
+                useNativeDriver: true,
+                duration: 1500
+            })
+        ).start()
+    },[width])
+    
+  return (
+    <View
+        style={parentContainerStyles}
+    >
+        <Animated.View
+            style={[styles.animatedView,{transform: [{translateX}]}]}
+        >
+            <LinearGradient 
+                colors={gradientColors}
+                start={gradientStartPosition}
+                style={styles.fillParent}
+            />
+        </Animated.View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: "rgba(0,0,0,0.12)", 
+        overflow: "hidden"
+    },
+    fillParent:{
+        width: "100%",
+        height: "100%"
+    },
+    animatedView:{
+        width: "100%",
+        height: "100%",
+        overflow: "hidden"
+    }
+})
